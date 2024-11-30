57a33925e89dc9aa7b3de0c94cddf70f33dabdf0
M.Kabiri <M.kabiri@choganitd.ir>
2024-11-30 20:04:19 +0330

    change the view

---
+++ CryptoQuoteApp/Helpers/CurrencyHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace CryptoQuoteApp.Helpers
+{
+    public static class CurrencyHelper
+    {
+        public static string FormatCurrency(string currencyCode, decimal amount)
+        {
+            CultureInfo culture;
+
+            switch (currencyCode)
+            {
+                case "EUR":
+                    culture = new CultureInfo("fr-FR");
+                    break;
+                case "BRL":
+                    culture = new CultureInfo("pt-BR");
+                    break;
+                case "GBP":
+                    culture = new CultureInfo("en-GB");
+                    break;
+                case "AUD":
+                    culture = new CultureInfo("en-AU");
+                    break;
+                default:
+                    culture = CultureInfo.InvariantCulture; // Default fallback to InvariantCulture
+                    break;
+            }
+
+            return string.Format(culture, "{0:C}", amount);
+        }
+
+    }
+}
