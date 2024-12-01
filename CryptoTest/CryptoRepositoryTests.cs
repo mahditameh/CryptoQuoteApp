0606e486900fd56a5e42be55fec417d23e499906
M.Kabiri <M.kabiri@choganitd.ir>
2024-12-01 11:41:14 +0330

    passed all tests

--- CryptoTest/CryptoRepositoryTests.cs
+++ CryptoTest/CryptoRepositoryTests.cs
@@ -1,44 +1,60 @@
 ﻿using CryptoTest.Stubs;
 using Infrastructure;
-using Microsoft.Extensions.Configuration;
+using Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
 using Moq;
 using System.Net;
-using System.Net.Http.Json;
+using System.Text;
 
 namespace CryptoTest
 {
     public class CryptoRepositoryTests
     {
-        private readonly Mock<IConfiguration> _mockConfiguration;
+        private readonly Mock<IOptions<ThirdPartySettings>> _mockOptions;
+        private readonly ThirdPartySettings _settings;
         private readonly string _baseCurrency = "USD";
 
         public CryptoRepositoryTests()
         {
-            _mockConfiguration = new Mock<IConfiguration>();
-            _mockConfiguration.Setup(cfg => cfg["CoinMarketCap:ApiKey"]).Returns("dummy_api_key");
+            // Mock ThirdPartySettings
+            _settings = new ThirdPartySettings
+            {
+                CoinMarketCap = new CoinMarketCapSettings
+                {
+                    ApiKey = "dummy_api_key",
+                    Url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+                },
+                ExchangeRates = new ExchangeRatesSettings
+                {
+                    ApiKey = "dummy_exchange_api_key",
+                    Url = "https://api.apilayer.com/exchangerates_data/latest?",
+                    Currencies = "EUR,BRL,GBP,AUD"
+                }
+            };
+
+            _mockOptions = new Mock<IOptions<ThirdPartySettings>>();
+            _mockOptions.Setup(o => o.Value).Returns(_settings);
         }
 
         [Fact]
         public async Task GetCryptoPriceAsync_ValidCryptoCode_ReturnsCorrectPrice()
         {
             // Arrange
+            var mockJsonResponse = new CryptoResponseBuilder()
+                 .AddCryptoPrice("BTC", 20000m)
+                 .Build();
+
             var mockResponse = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                Content = JsonContent.Create(new
-                {
-                    data = new
-                    {
-                        BTC = new { price = 20000 }
-                    }
-                })
+                Content = new StringContent(mockJsonResponse, Encoding.UTF8, "application/json")
             };
 
             var handler = new HttpMessageHandlerStub(mockResponse);
             var httpClient = new HttpClient(handler);
-            var repository = new CryptoRepository(httpClient, _mockConfiguration.Object);
+            var repository = new CryptoRepository(httpClient, _mockOptions.Object);
 
             // Act
-            var price = await repository.GetCryptoPriceAsync("BTC", _baseCurrency);
+            var price = await repository.GetCryptoPriceAsync("BTC", "USD");
 
             // Assert
             Assert.Equal(20000m, price);
@@ -51,7 +67,7 @@ namespace CryptoTest
             var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
             var handler = new HttpMessageHandlerStub(responseMessage);
             var httpClient = new HttpClient(handler);
-            var repository = new CryptoRepository(httpClient, _mockConfiguration.Object);
+            var repository = new CryptoRepository(httpClient, _mockOptions.Object);
 
             // Act & Assert
             await Assert.ThrowsAsync<HttpRequestException>(async () =>
---
+++ CryptoTest/Stubs/CryptoResponseBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace CryptoTest.Stubs
+{
+    public class CryptoResponseBuilder
+    {
+        private readonly Dictionary<string, object> _data;
+
+        public CryptoResponseBuilder()
+        {
+            _data = new Dictionary<string, object>();
+        }
+
+        public CryptoResponseBuilder AddCryptoPrice(string cryptoCode, decimal price)
+        {
+            _data[cryptoCode] = new
+            {
+                quote = new
+                {
+                    USD = new
+                    {
+                        price = price
+                    }
+                }
+            };
+            return this;
+        }
+
+        public string Build()
+        {
+            var response = new
+            {
+                data = _data
+            };
+
+            return JsonSerializer.Serialize(response);
+        }
+    }
+
+
+}
